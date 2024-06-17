b53c7620938aa8e4d92b0df5b398ecbf68962814
Lea9250 <lea.droguet@factorfx.com>
2024-06-17 11:23:13 +0200

    refactor: change plugin name in all files

--- install.php
+++ install.php
@@ -3,7 +3,7 @@
 /**
  * This function is called on installation and is used to create database schema for the plugin
  */
-function extension_install_UWPapps()
+function extension_install_uwpapps()
 {
     $commonObject = new ExtensionCommon;
 
@@ -24,16 +24,16 @@ function extension_install_UWPapps()
 /**
  * This function is called on removal and is used to destroy database schema for the plugin
  */
-function extension_delete_UWPapps()
+function extension_delete_uwpapps()
 {
     $commonObject = new ExtensionCommon;
-    $commonObject -> sqlQuery("DROP TABLE IF EXISTS `UWPapps`;");
+    $commonObject -> sqlQuery("DROP TABLE IF EXISTS `uwpapps`;");
 }
 
 /**
  * This function is called on plugin upgrade
  */
-function extension_upgrade_UWPapps()
+function extension_upgrade_uwpapps()
 {
 
 }
